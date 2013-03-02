f7841d354eee44743fe18cc47bb33ea38b5dcd87
Paul Silvis <paulsilvis@gmail.com>
2013-03-02 14:01:47 -0800

    initialized rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Looper::Application.config.secret_token = 'c89b513599bfd328c66e030b64e8164ee6b98aa8578a9816e859bbc6678c8f27d86348edc0f4d8ba1f6a0ee86983a72ad706f990ff261bde29b598679f90b4cd'
