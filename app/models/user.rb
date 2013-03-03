c85f9e42eaf3737df0c2e15d1b61b6d9ae381228
Paul Silvis <paulsilvis@gmail.com>
2013-03-02 18:38:13 -0800

    sessions fix for non-facebook-login

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   end
 
   def self.new_with_session(params, session)
-    if session
+    if session and session['devise.user_attributes']
       new(session['devise.user_attributes'], without_protection: true) do |user|
         user.attributes = params
         user.valid?
