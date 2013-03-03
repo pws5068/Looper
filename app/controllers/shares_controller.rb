8891e54cfc332c1a10b0ad42de2779b006969f67
Paul Silvis <paulsilvis@gmail.com>
2013-03-03 14:30:05 -0800

    whoooooops, typo (created videos on loads of shares.json)

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,7 +2,6 @@ class SharesController < ApplicationController
   # GET /shares
   # GET /shares.json
   def index
-    vid = Share.from_youtube 'Iuv__-nyO1M'
     @shares = Share.all
 
     respond_to do |format|
