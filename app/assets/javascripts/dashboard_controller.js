ebef4301e103c00e95f27afe5ffb3ede591baeaf
Matt Herzog <herzog.matt@gmail.com>
2013-03-03 14:28:03 -0800

    Added filtering in dashboard controller

--- app/assets/javascripts/dashboard_controller.js
+++ app/assets/javascripts/dashboard_controller.js
@@ -1,21 +1,65 @@
 function DashboardController($scope, $routeParams, $http) {
+	
 	$scope.groupId = $routeParams.groupId;
 	
-	// TODO: Show loader if local data is empty
-	$http.get('/shares').success(function(data) {
-		var shares = data;
-		for (var i = 0; i < data.length; i++) {
+	if ($scope.groupId != null) {
+		$http.get('/groups/' + $scope.groupId + '/shares').success(function(data) {
+			$scope.handleShares(data);
+		});		
+	} else {
+		// TODO: Show loader if local data is empty
+		$http.get('/shares').success(function(data) {
+			$scope.handleShares(data);
+		});		
+	}
+	
+	/**
+		Convenience methods for processing shares after HTTP callback
+	*/
+	$scope.handleShares = function(shares) {
+		for (var i = 0; i < shares.length; i++) {
 			var share = shares[i];
 			var groupId = share.group_id;
 			var group = Group.getGroupById(groupId);
 			share.group = group;
 		}
 		
-		$scope.shares = data;
-	});
+		$scope.allShares = shares;
+		$scope.filterAllShares();
+	};
 	
+	/*
+		When the user selects a share
+	*/
 	$scope.selectShare = function(share) {
 		// TODO: Expand the share
+	};
+	
+	/*
+		Sets the media type we are filtering
+	*/
+	$scope.setMediaType = function(mediaType) {
+		$scope.mediaType = mediaType;
+		$scope.filterAllShares();
+	};
+	
+	/*
+		All visible shares will be filtered right here
+	*/
+	$scope.filterAllShares = function() {
+		var filteredShares = [];
+		for (var i = 0; i < $scope.allShares.length; i++) {
+			var share = $scope.allShares[i];
+			if ($scope.mediaType) {
+				if (share.media_type == $scope.mediaType) {
+					filteredShares.push(share);				
+				}
+			} else {
+				filteredShares.push(share);
+			}
+		}
+		
+		$scope.shares = filteredShares;
 	}
 }
 
