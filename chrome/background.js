8216fa18d26b04008edbd892903d9bc74e3f52fe
Rick Harrison <rick.harrison@me.com>
2013-03-02 23:16:25 -0800

    Animates the main looper window onto the page.

--- chrome/background.js
+++ chrome/background.js
@@ -1,3 +1,5 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript(tab.id, { file: 'looper.js' });
+    chrome.tabs.executeScript(tab.id, { file: 'jquery.min.js' }, function() {
+        chrome.tabs.executeScript(tab.id, { file: 'looper.js' });
+    });
 });
