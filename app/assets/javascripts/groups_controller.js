575a53e3965d8665b7ac75d44c28803680b45388
Matt Herzog <herzog.matt@gmail.com>
2013-03-02 17:49:00 -0800

    Added groups controller

---
+++ app/assets/javascripts/groups_controller.js
@@ -0,0 +1,7 @@
+function GroupsController($scope) {
+	// To do load in groups
+	$scope.groups = [
+		{title:"Paul, Matt, Rick, and Zain", prettyDate:"yesterday", groupId:"100"},
+		{title:"Family", prettyDate:"1 week ago", groupId:"200"}
+	];
+}
