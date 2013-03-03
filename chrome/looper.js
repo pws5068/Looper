c485f77ce9bbdec2ec60eac2788d3ef0972638f4
Rick Harrison <rick.harrison@me.com>
2013-03-02 21:53:09 -0800

    Created the main looper files to be shown when the browser action is clicked.

--- chrome/looper.js
+++ chrome/looper.js
@@ -1,1 +1,23 @@
-alert('Hello World');
+/*
+ * Create the stylesheet
+ */
+
+var css =document.createElement("link")
+css.setAttribute("rel", "stylesheet")
+css.setAttribute("type", "text/css")
+css.setAttribute("href", chrome.extension.getURL('looper.css'));
+
+document.getElementsByTagName('body')[0].appendChild(css);
+
+/*
+ * Read in the main html
+ */
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", chrome.extension.getURL('looper.html'), true);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        document.body.innerHTML += xhr.response;
+    }
+};
+xhr.send();
