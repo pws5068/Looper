0dcd5c51fe303c5275fddb48add11b3712cda771
Paul Silvis <paulsilvis@gmail.com>
2013-03-03 01:01:31 -0800

    Added small,square icon photo to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130303065648) do
+ActiveRecord::Schema.define(:version => 20130303085924) do
 
   create_table "group_users", :force => true do |t|
     t.integer  "group_id"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(:version => 20130303065648) do
     t.string   "thumb_url"
     t.string   "oauth_token"
     t.string   "oauth_expires_at"
+    t.string   "icon_url"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
