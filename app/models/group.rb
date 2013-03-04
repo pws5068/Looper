05c9578a8c94852281893143860c39f999b60dc2
Paul Silvis <paulsilvis@gmail.com>
2013-03-03 22:24:13 -0800

    Ugly ass code that I pray nobody reads

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,4 +5,38 @@ class Group < ActiveRecord::Base
 
   attr_accessible :alias, :users
 
+  # @todo - Mega Hackish
+  def self.find_or_create( users )
+    user_ids = []
+    for user in users
+      user_ids << user.id
+    end
+
+    for group in users.first().groups
+      g_users = group.users
+
+      if g_users.count == user_ids.size
+        valid = true
+        for user in g_users
+          if !user_ids.include?(user.id)
+            valid = false
+          end
+        end
+        if valid
+          match = group
+          break
+        end
+      end
+    end
+
+    if !match
+      group = Group.create()
+      for user in users
+        GroupUser.create( :user_id => user.id, :group_id => group.id )
+      end
+    else
+      group = match
+    end
+    group
+  end
 end
