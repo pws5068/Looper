2749fbe80d9e984c250e7db7c98764198b3ddfdf
Paul Silvis <paulsilvis@gmail.com>
2013-03-04 00:42:42 -0800

    auto-scrape youtube data before_save()

--- app/models/share.rb
+++ app/models/share.rb
@@ -6,24 +6,38 @@ class Share < ActiveRecord::Base
   attr_accessible :url, :thumb, :title, :media_type, :description
   attr_accessible :preview_html, :share_views
 
-  validates :media_type, :inclusion => { :in => ['article', 'audio', 'video', 'photo'] }
+  #validates :media_type, :inclusion => { :in => ['article', 'audio', 'video', 'photo'] }
 
-  def self.from_youtube(identifier)
-    video = get_youtube_video( identifier )
+  before_save :scrape_data
+
+  def youtubeify( identifier )
+    video = Share.get_youtube_video( identifier )
     if video
-      share = Share.new()
-      share.title = video.title
-      share.description = video.description
-      share.thumb = video.thumbnails.first().url
-      share.url = video.player_url
-      share.preview_html = 
+      title = video.title
+      description = video.description
+      thumb = video.thumbnails.first().url
+      url = video.player_url
+      preview_html = 
       "<iframe id='ytplayer' type='text/html' width='640' height='390'
   src='http://www.youtube.com/embed/#{identifier}?autoplay=1'
   frameborder='0'/>" # @todo - this should be calculated on the fly
-      share.media_type = 'video'
-      share.save()
+      media_type = 'video'
     else
       false
+    end
+  end
+
+  def self.parse_youtube_url url
+    if url.nil?
+      false
+    else
+      regex = /^(?:http:\/\/)?(?:www\.)?\w*\.\w*\/(?:watch\?v=)?((?:p\/)?[\w\-]+)/
+      out = url.match(regex)
+      if ! out.nil?
+        out[1]
+      else
+        false
+      end
     end
   end
 
@@ -43,7 +57,20 @@ class Share < ActiveRecord::Base
     viewers
   end
 
+  def is_youtube?
+    !! Share.parse_youtube_url( url )
+  end
+
   private
+
+  def scrape_data
+    if is_youtube?
+      media_type = 'video'
+      identifier = Share.parse_youtube_url( url )
+      youtubeify( identifier )
+    end
+  end
+
   def self.get_youtube_video(identifier)
     client = yt_session()
     begin
@@ -52,6 +79,7 @@ class Share < ActiveRecord::Base
       false
     end
   end
+
   def self.yt_session
     # @todo - pull the following credentials out into a config file
     @yt_client ||= YouTubeIt::Client.new(
